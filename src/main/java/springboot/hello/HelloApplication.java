b1588a45f8a14e8a89d5bffcf7d60b24d4515210
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-02-11 16:02:46 +0900

    [Feat] GenericApplicationContext을 통해 스프링 컨테이너 생성 및 빈 초기화

--- src/main/java/springboot/hello/HelloApplication.java
+++ src/main/java/springboot/hello/HelloApplication.java
@@ -2,7 +2,7 @@ package springboot.hello;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,12 +16,17 @@ import java.io.IOException;
 public class HelloApplication {
 
     public static void main(String[] args) {
+
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        // 스프링 컨테이너에 빈 등록
+        applicationContext.registerBean(HelloController.class);
+        // 초기화하기
+        applicationContext.refresh();
+
         // 톰캣 사용(추상화가 잘 되어있어서 톰캣 대신 jetty로 바꿀 수도 있음)
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // 서블릿 컨테이너 생성
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            // 컨트롤러 추가
-            HelloController helloController = new HelloController();
 
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
@@ -30,19 +35,15 @@ public class HelloApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String msg = req.getParameter("message");
 
-                        // 서블릿의 비지니스 로직
+                        // 빈의 이름을 가져오는 방식 대신 클래스 빈의 타입이 1개만 있기에 클래스로 가져옴
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(msg);
 
-                        // 상태 라인, 상태 코드
-//                    resp.setStatus(200);
-                        resp.setStatus(HttpStatus.OK.value());
                         // 헤더(특히 Content-Type 헤더)
                         // resp.setHeader("Content-Type", "text/plain"); -> 오타 위험성
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         // 바디
                         resp.getWriter().println(ret);
-                    } else if (req.getRequestURI().equals("/member")) {
-
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
