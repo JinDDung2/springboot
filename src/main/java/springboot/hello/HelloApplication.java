377cad5cdc1e28eb2d83670afb5498a422f019ab
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-02-26 17:28:28 +0900

    [Feat] 스프링 컨테이너로 통합

--- src/main/java/springboot/hello/HelloApplication.java
+++ src/main/java/springboot/hello/HelloApplication.java
@@ -10,23 +10,30 @@ public class HelloApplication {
     public static void main(String[] args) {
 
         // DispatcherServlet 을 사용하기위해 GenericApplicationContext -> GenericWebApplicationContext
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+            TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+            // 서블릿 컨테이너 생성
+            WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+                servletContext.addServlet("dispatcherServlet",
+                        // 기존에는 ApplicationContext 객체를 생성해서 변수에 넣고, 변수를 servlet 컨테이너를 초기화 할 때 가져와서 사했는데
+                        // 지금은 확장하고자 하는 클래스 내부에 ApplicationContext를 참조하게 해야하니까 자기 자신을 참조
+                        new DispatcherServlet(this)
+                        ).addMapping("/*");
+            });
+            webServer.start();
+            }
+        };
         // 스프링 컨테이너에 빈 등록
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
-        // 초기화하기
+        // 스프링 컨테이너 초기화 작업 -> refresh()로 함
         applicationContext.refresh();
 
-        // 톰캣 사용(추상화가 잘 되어있어서 톰캣 대신 jetty로 바꿀 수도 있음)
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        // 서블릿 컨테이너 생성
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-            servletContext.addServlet("dispatcherServlet",
-                    new DispatcherServlet(applicationContext)
-                    ).addMapping("/*");
-        });
-        webServer.start();
     }
 
 }
