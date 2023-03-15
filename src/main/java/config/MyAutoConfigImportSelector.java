d2435ace097c32a7a0155b06d5b8348c6e457010
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-03-15 17:04:04 +0900

    [Feat] 자동 구성 설정 파일 분리

--- src/main/java/config/MyAutoConfigImportSelector.java
+++ src/main/java/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,32 @@
 package config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+//    @Override
+//    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+//        Iterable<String> candidates = ImportCandidates.load(MyAutoConfig.class, classLoader);
+//        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
+//    };
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "config.autoconfig.DispatcherServletConfig",
-                "config.autoconfig.TomcatWebServerConfig"
-        };
-    }
+        List<String> autoConfigs = new ArrayList<>();
+        ImportCandidates.load(MyAutoConfig.class, classLoader).forEach(candidate ->
+                autoConfigs.add(candidate)
+        );
+
+        return autoConfigs.stream().toArray(String[]::new);
+    };
 }
