a80d6a1f6cd805ebe2030263a11d6e4a4ea86b03
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-03-20 16:17:12 +0900

    [Feat] jetty Web Servlet 추가

--- src/main/java/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/config/autoconfig/JettyWebServerConfig.java
@@ -1,14 +1,14 @@
 package config.autoconfig;
 
 import config.MyAutoConfig;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfig
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory")
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
