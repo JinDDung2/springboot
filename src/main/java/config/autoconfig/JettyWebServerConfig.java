7d84ed1fd351ff903619a8e560ceaa0607e7e886
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-03-20 16:55:56 +0900

    [Test] Conditional 학습 테스트

--- src/main/java/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/config/autoconfig/JettyWebServerConfig.java
@@ -4,11 +4,24 @@ import config.MyAutoConfig;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfig
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition {
+        @Override
+        // AnnotatedTypeMetadata -> Conditional (메타)애노테이션을 사용하고 있는 그 위치에 붙은 다른 애노테이션 정보를 이용할 수 있음
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
---
+++ src/test/java/springboot/study/ConditionalTest.java
@@ -0,0 +1,78 @@
+package springboot.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConditionalTest {
+    @Test
+    void conditional() throws Exception {
+        // true
+        // 테스트용 ApplicationContextRunner
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner();
+        contextRunner.withUserConfiguration(Config1.class)
+                .run(context -> {
+                    assertThat(context).hasSingleBean(MyBean.class);
+                    assertThat(context).hasSingleBean(Config1.class);
+                });
+        // false
+        new ApplicationContextRunner().withUserConfiguration(Config2.class)
+                .run(context -> {
+                    assertThat(context).doesNotHaveBean(Config1.class);
+                    assertThat(context).doesNotHaveBean(MyBean.class);
+                });
+        
+    }
+
+    @Configuration
+    @TrueConditional
+    static class Config1 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    @Configuration
+    @FalseConditional
+    static class Config2 {
+        @Bean
+        MyBean myBean() {
+            return new MyBean();
+        }
+    }
+
+    static class MyBean {}
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(TrueCondition.class)
+    @interface TrueConditional {}
+
+    private static class TrueCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    @Conditional(FalseCondition.class)
+    @interface FalseConditional {}
+
+    private static class FalseCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
+    }
+}
