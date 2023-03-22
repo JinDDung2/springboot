faaea147716c99a0289cece0723518bd0a877a96
Jin-hyuck <gjwlsgur4866@gmail.com>
2023-03-22 15:29:21 +0900

    [Feat] 자동 구성 정보를 내가 만든 구성정보로 대체 (단 연결된 빈들의 요소들을 잘 알고 나서 해야함)

--- src/main/java/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/config/autoconfig/JettyWebServerConfig.java
@@ -1,6 +1,7 @@
 package config.autoconfig;
 
 import config.MyAutoConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfig
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
+    @ConditionalOnMissingBean
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
--- src/main/java/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/config/autoconfig/TomcatWebServerConfig.java
@@ -1,6 +1,7 @@
 package config.autoconfig;
 
 import config.MyAutoConfig;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,9 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    // @ConditionalMyOnClass이 먼저 통과 됨 -> 여기서 통과되지 않는다면 어차피 아래 애노테이션은 시작도 안됨
+    // ConditionalMyOnClass이 웹 클래스 레벨을 통과 하면 아래 타입의 빈이 사용자가 직접 정의하지 않았다면 빈으로 등록
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
